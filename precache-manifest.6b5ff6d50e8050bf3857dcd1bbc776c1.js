a1504540f5c9bcc257a604cdb8b595afaf54d672
mohidul0901 <mohidulsagu@gmail.com>
2020-09-18 14:15:02 +0600

    Updates

---
+++ precache-manifest.6b5ff6d50e8050bf3857dcd1bbc776c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4fcf4d69803ba2b15ac9c3a3af668b",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "fa904147f0f480d1723b",
+    "url": "/Covid-19-Tracker/static/css/main.4743abcc.chunk.css"
+  },
+  {
+    "revision": "88c50e7a7734a14e108c",
+    "url": "/Covid-19-Tracker/static/js/2.6bf1a85b.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid-19-Tracker/static/js/2.6bf1a85b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa904147f0f480d1723b",
+    "url": "/Covid-19-Tracker/static/js/main.6e3443fb.chunk.js"
+  },
+  {
+    "revision": "4b644fa9ca0287604fad",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.b2c8fe1b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
